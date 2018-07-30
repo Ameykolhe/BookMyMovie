7a168fc46c5add1d32fc609d84c0bfe20a96aa8a
kolheamey99@gmail.com <kolheamey99@gmail.com>
2018-07-30 22:50:16 +0530

    Server Side Owner Handler

---
+++ src/com/ameykolhe/bookmymovie/server/OwnerHandler.java
@@ -0,0 +1,45 @@
+package com.ameykolhe.bookmymovie.server;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+import java.net.Socket;
+
+public class OwnerHandler implements Runnable {
+
+	Socket s;
+	DataInputStream dis;
+	DataOutputStream dos;
+	ObjectOutputStream oos;
+	ObjectInputStream ois;
+	
+	String userName;
+	String password;
+	Integer type;
+	
+	public OwnerHandler(Socket s,DataInputStream dis,DataOutputStream dos,ObjectInputStream ois,ObjectOutputStream oos,String userName , String password , Integer type)
+	{
+		this.dis = dis;
+		this.dos = dos;
+		this.ois = ois;
+		this.oos = oos;
+		this.s = s;
+		this.userName = userName;
+		this.password = password;
+		this.type = type;
+	}
+	
+	
+	@Override
+	public void run() {
+		System.out.println("Starting Thread : "+Thread.currentThread().getName());
+		
+		System.out.println("IN OwnerHandeler Thread");
+		
+		System.out.println("Stopping Thread  : " + Thread.currentThread().getName());
+		
+	}
+
+}
