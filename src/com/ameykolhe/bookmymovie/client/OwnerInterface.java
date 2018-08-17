db3794969b8f00596a75642a2c088c248675fdc9
kolheamey99@gmail.com <kolheamey99@gmail.com>
2018-08-17 19:06:28 +0530

    scheduleMovie

--- src/com/ameykolhe/bookmymovie/client/OwnerInterface.java
+++ src/com/ameykolhe/bookmymovie/client/OwnerInterface.java
@@ -2,11 +2,12 @@ package com.ameykolhe.bookmymovie.client;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
 import java.net.Socket;
-
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class OwnerInterface implements Runnable{
@@ -29,39 +30,103 @@ public class OwnerInterface implements Runnable{
 	}
 	
 	
+	@SuppressWarnings({ "unchecked", "unused" })
+	void scheduleMovie() throws ClassNotFoundException, IOException
+	{
+		String movieName = null;
+		ArrayList<String> movieNames = (ArrayList<String>)ois.readObject();
+		System.out.println("Enter Movie Name : ");
+		
+		for(int i = 0 ; i<movieNames.size() ;i++ )
+		{
+			System.out.println( (i + 1) + "   " + movieNames.get(i) );
+		}
+		dos.writeUTF(sc.next());
+		
+		ArrayList<String> theatreNames = (ArrayList<String>)ois.readObject();
+		System.out.println("Enter Theatre Name : ");
+		
+		for(int i = 0 ; i<theatreNames.size() ;i++ )
+		{
+			System.out.println( (i + 1) + "   " + theatreNames.get(i) );
+		}
+		dos.writeUTF(sc.next());
+		
+		ArrayList<Integer> screenNum = (ArrayList<Integer>)ois.readObject();
+		System.out.println("Enter Screen Num : ");
+		
+		for(int i = 0 ; i<screenNum.size() ;i++ )
+		{
+			System.out.print( "\t" + screenNum.get(i) );
+		}
+		System.out.println();
+		dos.writeUTF(sc.next());
+		
+		System.out.print("Enter Date (yyyy-mm-dd) :  ");
+		dos.writeUTF(sc.next());
+		System.out.print("Enter Time (HH:MM) :  ");
+		dos.writeUTF(sc.next() + ":00");
+		
+		System.out.println("Enter Price  : ");
+		dos.writeUTF(sc.next());
+		
+		System.out.println(dis.readUTF());
+		
+	}
+	
+	
+	void createNewTheatre()
+	{
+		
+	}
+	
 	@Override
 	public void run() {
 		System.out.println("Starting Thread : "+Thread.currentThread().getName());
 		
 		System.out.println("In Owner Interface");
-	
+		
+		boolean flag = false;
+		
+		while(true)
+		{
+			System.out.println("Enter : 1: Schedule Movie\n\t2: Create new Theatre\n\t3: Exit");
+			int choice = sc.nextInt();
+			switch(choice)
+			{
+				case 1:
+					try {
+						dos.writeUTF("1");
+						scheduleMovie();
+					
+					} catch (IOException | ClassNotFoundException e1) {
+						e1.printStackTrace();
+					}
+					break;
+					
+				case 2:
+					
+					
+					
+				case 3:
+					try {
+						dos.writeUTF("3");
+						flag = true;
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+					break;
+			}
+			
+			if(flag)
+			{
+				break;
+			}
+		}
+		
 	
 	
 		System.out.println("Stopping Thread  : " + Thread.currentThread().getName());
-	}
-	
-	
-	
-	
-	final static void clearConsole()
-	{
-	    try
-	    {
-	        final String os = System.getProperty("os.name");
-
-	        if (os.contains("Windows"))
-	        {
-	            Runtime.getRuntime().exec("cls");
-	        }
-	        else
-	        {
-	            Runtime.getRuntime().exec("clear");
-	        }
-	    }
-	    catch (final Exception e)
-	    {
-	        //  Handle any exceptions.
-	    }
 	}
 
 }
--- src/com/ameykolhe/bookmymovie/server/OwnerHandler.java
+++ src/com/ameykolhe/bookmymovie/server/OwnerHandler.java
@@ -2,10 +2,16 @@ package com.ameykolhe.bookmymovie.server;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
 import java.net.Socket;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import com.ameykolhe.bookmymovie.utilities.DatabaseMGR;
 
 public class OwnerHandler implements Runnable {
 
@@ -18,8 +24,9 @@ public class OwnerHandler implements Runnable {
 	String userName;
 	String password;
 	Integer type;
+	Integer ownerID;
 	
-	public OwnerHandler(Socket s,DataInputStream dis,DataOutputStream dos,ObjectInputStream ois,ObjectOutputStream oos,String userName , String password , Integer type)
+	public OwnerHandler(Socket s,DataInputStream dis,DataOutputStream dos,ObjectInputStream ois,ObjectOutputStream oos,String userName , String password , Integer type , Integer ownerID)
 	{
 		this.dis = dis;
 		this.dos = dos;
@@ -29,16 +36,152 @@ public class OwnerHandler implements Runnable {
 		this.userName = userName;
 		this.password = password;
 		this.type = type;
+		this.ownerID = ownerID;
 	}
+	
+	
+	@SuppressWarnings({ "unused", "null", "deprecation" })
+	void scheduleMovie() throws SQLException, IOException
+	{
+		System.out.println("In Schedule Movie");
+		String query = "Select * from movie";
+		ResultSet rs = DatabaseMGR.executeQuery(query);
+		
+		ArrayList <String> movieNames = new ArrayList<String>();
+		ArrayList <Integer> movieID = new ArrayList<Integer>();
+		ArrayList <String> theatreNames = new ArrayList<String>();
+		ArrayList <Integer> theatreID = new ArrayList<Integer>();
+		ArrayList <Integer> screenNum = new ArrayList<Integer>();
+		ArrayList <Integer> screenID = new ArrayList<Integer>();
+		
+		while(rs.next())
+		{
+			movieNames.add(rs.getString("movieName"));
+			movieID.add(rs.getInt("movieID"));
+		}
+		oos.writeObject(movieNames);
+		
+		Integer movieIndex = Integer.parseInt(dis.readUTF().toString());
+		movieIndex = movieIndex - 1; 
+		System.out.println("movieName : " + movieNames.get(movieIndex));
+		
+		query = "Select * from theatre Where ownerID = " + this.ownerID + ";";
+		rs = DatabaseMGR.executeQuery(query);
+		
+		while(rs.next())
+		{
+			theatreNames.add(rs.getString("theatreName"));
+			theatreID.add(rs.getInt("theatreID"));
+		}
+		oos.writeObject(theatreNames);
+		
+		Integer theatreIndex = Integer.parseInt(dis.readUTF().toString());
+		theatreIndex = theatreIndex - 1;
+		
+		System.out.println("theatreName : " + theatreNames.get(theatreIndex));
+		
+		query = "Select * from screen Where theatreID = " + theatreID.get(theatreIndex) + ";" ;
+		rs = DatabaseMGR.executeQuery(query);
+		
+		while(rs.next())
+		{
+			screenNum.add(rs.getInt("screenNum"));
+			screenID.add(rs.getInt("screenID"));
+		}
+		oos.writeObject(screenNum);
+		
+		Integer screenIndex = Integer.parseInt(dis.readUTF().toString());
+		screenIndex = screenIndex - 1;
+		
+		System.out.println("screenNum : " + screenNum.get(screenIndex));
+		
+		String showDate = dis.readUTF().toString();
+		
+		String time = dis.readUTF().toString();
+		
+		String showTime = showDate + " " + time;
+		
+		System.out.println("ShowTIme : " + showTime);
+		
+		Integer price = Integer.parseInt(dis.readUTF().toString());
+		Integer stat = new Integer(0);
+		
+		query = String.format( "Select scheduleID from schedule where screenId = %d and (showTime between '%s' and ADDTIME('%s',(select runTime from movie where movieID = %d)) );" ,screenID.get(screenIndex) , showTime , showTime , movieID.get(movieIndex) );
+		rs = DatabaseMGR.executeQuery(query);
+		if(rs.next())
+		{
+			stat = Integer.parseInt("-1");
+		}
+		if(stat.intValue() != -1)
+		{
+			query = String.format("Insert into schedule(showDate,showTime,screenID,movieID,price) values ('%s','%s',%d,%d,%d);",showDate,showTime,screenID.get(screenIndex),movieID.get(movieIndex),price);
+			//query = "Insert into schedule(showDate,showTime,screenID,movieID,price) values ("+" ' " + showDate + " ',' "+showTime+" ', " + screenID.get(screenIndex).toString()+" , "+movieID.get(movieIndex).toString()+" , "+price.toString()+" ); ";
+			stat = DatabaseMGR.executeUpdate(query);
+			if(stat.intValue() == 1 )
+			{
+				dos.writeUTF("Movie Successfully Scheduled");
+			}
+			else
+			{
+				dos.writeUTF("Schedule Unsuccessful");
+			}
+		}
+		else
+		{
+			dos.writeUTF("Schedule Unsuccessful : SCREEN IS BUSY");
+		}
+		
+	}
+	
+	
 	
 	
 	@Override
 	public void run() {
 		System.out.println("Starting Thread : "+Thread.currentThread().getName());
 		
-		System.out.println("IN OwnerHandeler Thread");
+		System.out.println("IN OwnerHandler Thread");
+		
+		
+		boolean flag = false;
+		
+		while(true)
+		{
+			Integer choice = null;
+			try {
+				choice = Integer.parseInt( dis.readUTF().toString() );
+			} catch (NumberFormatException | IOException e) {
+				e.printStackTrace();
+			}
+			switch(choice)
+			{
+				case 1:
+					try {
+						scheduleMovie();
+					} catch (SQLException | IOException e) {
+						e.printStackTrace();
+					}	
+					break;
+				case 2:
+					
+					
+					break;
+				case 3:
+					flag = true;
+					break;
+			}
+			
+			if(flag)
+			{
+				break;
+			}
+		}
+		
+		
+		
 		
 		System.out.println("Stopping Thread  : " + Thread.currentThread().getName());
+		
 		
 	}
 
