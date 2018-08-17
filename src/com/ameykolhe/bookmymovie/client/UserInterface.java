d3aba07d9c84bac370cf0a97dd7e7fea750d19a4
kolheamey99@gmail.com <kolheamey99@gmail.com>
2018-08-17 19:06:43 +0530

    bookTicket

--- src/com/ameykolhe/bookmymovie/client/UserInterface.java
+++ src/com/ameykolhe/bookmymovie/client/UserInterface.java
@@ -1,12 +1,17 @@
 package com.ameykolhe.bookmymovie.client;
 
+import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
 import java.net.Socket;
-
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class UserInterface implements Runnable{
@@ -28,9 +33,88 @@ public class UserInterface implements Runnable{
 		this.s = s;
 	}
 	
-	void bookTicket()
-	{
+	@SuppressWarnings({ "unchecked", "deprecation", "unused" })
+	void bookTicket() throws IOException, ClassNotFoundException, ParseException, InterruptedException
+	{	
+		ArrayList <String> cityNames = (ArrayList<String>)ois.readObject();
+		System.out.println("Select City : ");
+		for(int i = 0 ; i<cityNames.size() ;i++ )
+		{
+			System.out.println( (i + 1) + "   " + cityNames.get(i) );
+		}
+		String cityIndex = sc.next();
+		dos.writeUTF(cityIndex);
 		
+		ArrayList <String> movieNames = (ArrayList<String>)ois.readObject();	
+		System.out.println("Select Movie : ");
+		for(int i = 0 ; i<movieNames.size() ;i++ )
+		{
+			System.out.println( (i + 1) + "   " + movieNames.get(i) );
+		}
+		String movieIndex = sc.next();
+		dos.writeUTF(movieIndex);
+		
+		ArrayList<String> scheduleDates = (ArrayList <String>) ois.readObject();
+		System.out.println("Select Date : ");
+		for(int i = 0 ; i<scheduleDates.size() ;i++ )
+		{
+			System.out.println( (i + 1) + "   " + scheduleDates.get(i) );
+		}
+		String scheduleDateIndex = sc.next();
+		dos.writeUTF(scheduleDateIndex);
+		
+		ArrayList<String> theatreNames = (ArrayList <String>) ois.readObject();
+		System.out.println("Select theatreName : ");
+		for(int i = 0 ; i < theatreNames.size() ;i++ )
+		{
+			System.out.println( (i + 1) + "   " + theatreNames.get(i) );
+		}
+		String theatreIndex = sc.next();
+		dos.writeUTF(theatreIndex);
+		
+		ArrayList<String> showTimes = (ArrayList <String>) ois.readObject();
+		System.out.println("Select showTime : ");
+		for(int i = 0 ; i < showTimes.size() ;i++ )
+		{
+			System.out.println( (i + 1) + "   " + showTimes.get(i) );
+		}
+		String showTimeIndex = sc.next();
+		dos.writeUTF(showTimeIndex);
+		
+		Integer rows = (Integer)ois.readObject();
+		Integer col = (Integer)ois.readObject();
+		ArrayList <Integer> availableSeatNO = (ArrayList <Integer>)ois.readObject();
+		System.out.println("Avaliable seats : \n");
+		for(int i = 1 ; i <= rows.intValue()*col.intValue() ; i++ )
+		{
+			if(availableSeatNO.contains(i))
+			{
+				System.out.print(String.format("%3d ", i));
+			}
+			else
+			{
+				System.out.print(" X  ");
+			}
+			if(i%col.intValue() == 0)
+			{
+				System.out.println();
+			}
+		}
+		
+		System.out.println("\nEnter Seat NO : ");
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String seatNo = br.readLine();
+		ArrayList<String> seatNOs = new ArrayList<String>(Arrays.asList(seatNo.split(" ")));
+		oos.writeObject(seatNOs);
+		
+		String msg = dis.readUTF().toString();
+		if(msg.equals("Booking Successful"))
+		{
+			Integer bookingID = dis.readInt();
+			System.out.println("--------------------Booking Details----------------------- ");
+			System.out.println("BookingID : " + bookingID.toString());
+			Thread.sleep(1000);
+		}
 	}
 	
 	
@@ -49,20 +133,35 @@ public class UserInterface implements Runnable{
 		boolean flag = false;
 		while(true)
 		{
-			System.out.println("Enter  : 1: Book Ticket\n\t2: Show Bookings\n\t3: Exit");
+			System.out.println("Enter : 1: Book Ticket\n\t2: Show Bookings\n\t3: Exit");
 			choice = sc.nextInt();
 			
 			switch(choice)
 			{
 				case 1:
-					bookTicket();
+					try {
+						dos.writeUTF("1");
+						bookTicket();
+					} catch (IOException | ClassNotFoundException | ParseException | InterruptedException e) {
+						e.printStackTrace();
+					}
 					break;
 					
 				case 2:
-					showBookings();
+					try {
+						dos.writeUTF("2");
+						showBookings();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
 					break;
 					
 				case 3:
+					try {
+						dos.writeUTF("3");
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
 					flag = true;
 					break;
 			}
@@ -71,33 +170,8 @@ public class UserInterface implements Runnable{
 			{
 				break;
 			}
-			clearConsole();
 		}
 		System.out.println("Stopping Thread  : " + Thread.currentThread().getName());
 	}
 	
-	
-	
-	
-	final static void clearConsole()
-	{
-	    try
-	    {
-	        final String os = System.getProperty("os.name");
-
-	        if (os.contains("Windows"))
-	        {
-	            Runtime.getRuntime().exec("cls");
-	        }
-	        else
-	        {
-	            Runtime.getRuntime().exec("clear");
-	        }
-	    }
-	    catch (final Exception e)
-	    {
-	        //  Handle any exceptions.
-	    }
-	
-	}
 }
--- src/com/ameykolhe/bookmymovie/server/UserHandler.java
+++ src/com/ameykolhe/bookmymovie/server/UserHandler.java
@@ -2,10 +2,16 @@ package com.ameykolhe.bookmymovie.server;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
 import java.net.Socket;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import com.ameykolhe.bookmymovie.utilities.DatabaseMGR;
 
 public class UserHandler implements Runnable {
 
@@ -18,8 +24,9 @@ public class UserHandler implements Runnable {
 	String userName;
 	String password;
 	Integer type;
+	Integer userID;
 	
-	public UserHandler(Socket s,DataInputStream dis,DataOutputStream dos,ObjectInputStream ois,ObjectOutputStream oos,String userName , String password , Integer type)
+	public UserHandler(Socket s,DataInputStream dis,DataOutputStream dos,ObjectInputStream ois,ObjectOutputStream oos,String userName , String password , Integer type , Integer userID)
 	{
 		this.dis = dis;
 		this.dos = dos;
@@ -29,13 +36,188 @@ public class UserHandler implements Runnable {
 		this.userName = userName;
 		this.password = password;
 		this.type = type;
+		this.userID = userID;
 	}
+	
+	
+	@SuppressWarnings({ "deprecation", "unchecked", "unused" })
+	void bookTicket() throws IOException, SQLException, ClassNotFoundException
+	{
+		System.out.println("In bookTicket");
+		
+		
+		String query = "SELECT * from city;";
+		ArrayList<String> cityNames = new ArrayList<String>();
+		ArrayList<Integer> cityID = new ArrayList<Integer>();
+		ResultSet rs = DatabaseMGR.executeQuery(query);
+		while(rs.next())
+		{
+			cityNames.add(rs.getString("cityName"));
+			cityID.add(rs.getInt("cityID"));
+		}
+		oos.writeObject(cityNames);
+		Integer cityIndex = Integer.parseInt(dis.readUTF().toString());
+		cityIndex = cityIndex - 1;
+		
+		
+		query = String.format("select distinct movie.movieID , movie.movieName from schedule inner join movie on(schedule.movieID = movie.MovieID) inner join screen on (schedule.screenID = screen.screenID) inner join theatre on(theatre.theatreID = screen.screenID) inner join city on (theatre.cityID = city.cityID)  where schedule.showTime > current_timestamp and city.cityID = %d;",cityID.get(cityIndex.intValue()));
+		rs = DatabaseMGR.executeQuery(query);
+		ArrayList<String> movieNames = new ArrayList <String >();
+		ArrayList <Integer> movieID = new ArrayList <Integer>();
+		while(rs.next())
+		{
+			movieNames.add(rs.getString("movie.movieName"));
+			//System.out.println(rs.getString("movie.movieName"));
+			movieID.add(rs.getInt("movie.movieID"));
+			//System.out.println(rs.getString("movie.movieID"));
+		}
+		oos.writeObject(movieNames);
+		Integer movieIndex = Integer.parseInt(dis.readUTF().toString());
+		movieIndex = movieIndex - 1;
+		
+		
+		query = String.format("select distinct schedule.showDate from schedule inner join movie on(schedule.movieID = movie.MovieID) inner join screen on (schedule.screenID = screen.screenID) inner join theatre on(theatre.theatreID = screen.theatreID) inner join city on (theatre.cityID = city.cityID)  where schedule.showTime > current_timestamp and city.cityID = %d and movie.movieID = %d;",cityID.get(cityIndex.intValue()),movieID.get(movieIndex.intValue()));
+		rs = DatabaseMGR.executeQuery(query);
+		
+		ArrayList <String> scheduleDates = new ArrayList <String>();
+		while(rs.next())
+		{
+			//System.out.println(rs.getString("schedule.showDate"));
+			scheduleDates.add(rs.getString("schedule.showDate"));
+		}
+		oos.writeObject(scheduleDates);
+		Integer scheduleDateIndex = Integer.parseInt(dis.readUTF().toString());
+		scheduleDateIndex = scheduleDateIndex - 1; 
+		
+		
+		query = String.format( "select distinct theatre.theatreID , theatre.theatreName from schedule inner join movie on(schedule.movieID = movie.MovieID) inner join screen on (schedule.screenID = screen.screenID) inner join theatre on(theatre.theatreID = screen.theatreID) inner join city on (theatre.cityID = city.cityID)  where schedule.showTime > current_timestamp and city.cityID = %d and movie.movieID = %d and schedule.showDate = '%s';",cityID.get(cityIndex.intValue()),movieID.get(movieIndex.intValue()) , scheduleDates.get(scheduleDateIndex.intValue()) );
+		rs = DatabaseMGR.executeQuery(query);
+		ArrayList <String> theatreNames = new ArrayList <String>();
+		ArrayList <Integer> theatreID = new ArrayList <Integer>();
+		while(rs.next())
+		{
+			theatreNames.add(rs.getString("theatre.theatreName"));
+			//System.out.println(rs.getString("theatre.theatreName"));
+			theatreID.add(rs.getInt("theatre.theatreID"));
+			//System.out.println(rs.getString("theatre.theatreID"));
+		}
+		oos.writeObject(theatreNames);
+		Integer theatreIndex = Integer.parseInt(dis.readUTF().toString());
+		theatreIndex  = theatreIndex - 1;
+		
+		
+		query = String.format( "select schedule.showTime , schedule.scheduleID , schedule.screenID from schedule inner join movie on(schedule.movieID = movie.MovieID) inner join screen on (schedule.screenID = screen.screenID) inner join theatre on(theatre.theatreID = screen.theatreID) inner join city on (theatre.cityID = city.cityID)  where schedule.showTime > current_timestamp and city.cityID = %d and movie.movieID = %d and schedule.showDate = '%s' and theatre.theatreID = %d;",cityID.get(cityIndex.intValue()),movieID.get(movieIndex.intValue()) , scheduleDates.get(scheduleDateIndex.intValue()),theatreID.get(theatreIndex.intValue()) );
+		rs = DatabaseMGR.executeQuery(query);
+		ArrayList <String> showTime = new ArrayList <String>();
+		ArrayList <Integer> scheduleID = new ArrayList <Integer>();
+		ArrayList <Integer> screenID = new ArrayList <Integer>();
+		while(rs.next())
+		{
+			showTime.add(rs.getString("schedule.showTime"));
+			scheduleID.add(rs.getInt("schedule.scheduleID"));
+			screenID.add(rs.getInt("schedule.screenID"));
+		}
+		oos.writeObject(showTime);
+		Integer scheduleIndex = Integer.parseInt(dis.readUTF().toString());
+		scheduleIndex = scheduleIndex - 1;
+		
+		//System.out.println(scheduleID.get(scheduleIndex.intValue()));
+		
+		query = String.format("Select * from seatmap where screenID = %d and seatmapID not in (select bookedseats.seatmapID from bookedseats NATURAL JOIN bookinginfo where scheduleID = %d )", screenID.get(scheduleIndex.intValue()),scheduleID.get(scheduleIndex.intValue()));
+		rs = DatabaseMGR.executeQuery(query);
+		ArrayList<Integer> availableSeatNO = new ArrayList <Integer>();
+		ArrayList<Integer> availableSeatMapID = new ArrayList<Integer>();
+		while(rs.next())
+		{
+			availableSeatNO.add(rs.getInt("seatNO"));
+			availableSeatMapID.add(rs.getInt("seatMapID"));
+		}
+		query = String.format("Select noOfRows , noOfCol from schedule NATURAL JOIN screen where scheduleID = %d and screen.screenID = %d", scheduleID.get(scheduleIndex.intValue()),screenID.get(scheduleIndex.intValue()));
+		rs = DatabaseMGR.executeQuery(query);
+		Integer rows = null;
+		Integer col = null;
+		while(rs.next())
+		{
+			rows = new Integer(rs.getInt("noOfRows"));
+			col = new Integer(rs.getInt("noOfCol"));
+		}
+		oos.writeObject(rows);
+		oos.writeObject(col);
+		oos.writeObject(availableSeatNO);
+		
+		
+		ArrayList <String> seatNO = (ArrayList <String>) ois.readObject();
+		for(int i = 0 ; i<seatNO.size() ;i++)
+		{
+			System.out.println(seatNO.get(i));
+		}
+		
+		query = String.format("Insert into bookinginfo( userID,scheduleID,totalSeats,bookingTime ) values(%d,%d,%d,current_timestamp)", this.userID,scheduleID.get(scheduleIndex.intValue()) , seatNO.size() );
+		DatabaseMGR.executeUpdate(query);
+		query = String.format("Select bookingID from bookinginfo where scheduleID = %d and userID = %d", scheduleID.get(scheduleIndex.intValue()),this.userID);
+		rs = DatabaseMGR.executeQuery(query);
+		rs.next();
+		Integer bookingID = rs.getInt("bookingID");
+		for(int i = 0 ; i<seatNO.size() ; i++)
+		{
+			query = String.format("Insert into bookedseats(bookingID,seatmapID) values(%d,%d)", bookingID.intValue() , availableSeatMapID.get( Integer.parseInt(seatNO.get(i)) - 1 ) );
+			DatabaseMGR.executeUpdate(query);
+		}
+		
+		dos.writeUTF("Booking Successful");
+		dos.writeInt(bookingID);
+	}
+	
+	
+	void showBookings() 
+	{
+		System.out.println("In Show Bookings");
+		
+	}
+	
+	
 	
 	@Override
 	public void run() {
 		System.out.println("Starting Thread : "+Thread.currentThread().getName());
 		
-		System.out.println("IN UserHandeler Thread");
+		System.out.println("IN UserHandler Thread");
+		
+		boolean flag = false;
+		
+		while(true)
+		{
+			Integer choice = null;
+			try {
+				choice = Integer.parseInt( dis.readUTF().toString() );
+			} catch (NumberFormatException | IOException e) {
+				e.printStackTrace();
+			}
+			switch(choice)
+			{
+				case 1:
+					try {
+						bookTicket();
+					} catch (SQLException | IOException | ClassNotFoundException e) {
+						e.printStackTrace();
+					}	
+					break;
+					
+				case 2:
+					showBookings();
+					break;
+					
+				case 3:
+					flag = true;
+					break;
+			}
+			
+			if(flag)
+			{
+				break;
+			}
+		}
+		
 		
 		System.out.println("Stopping Thread  : " + Thread.currentThread().getName());
 		
