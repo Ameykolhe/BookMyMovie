17ba8159d5a861add3e85101137c825edad7331e
kolheamey99@gmail.com <kolheamey99@gmail.com>
2018-08-21 10:40:33 +0530

    BookTicketPanel

---
+++ src/com/ameykolhe/bookmymovie/userInterface/BookTicket.java
@@ -0,0 +1,19 @@
+package com.ameykolhe.bookmymovie.userInterface;
+
+import javax.swing.JPanel;
+
+public class BookTicket extends JPanel implements Runnable{
+
+	private static final long serialVersionUID = 1L;
+
+	public BookTicket() {
+
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
