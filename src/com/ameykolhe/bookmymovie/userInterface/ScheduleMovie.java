dda54527f5397124ed073fa0e56da00c9b3888ed
kolheamey99@gmail.com <kolheamey99@gmail.com>
2018-08-21 10:40:49 +0530

    ScheduleMoviePanel

---
+++ src/com/ameykolhe/bookmymovie/userInterface/ScheduleMovie.java
@@ -0,0 +1,19 @@
+package com.ameykolhe.bookmymovie.userInterface;
+
+import javax.swing.JPanel;
+
+public class ScheduleMovie extends JPanel implements Runnable{
+
+	private static final long serialVersionUID = 1L;
+	
+	public ScheduleMovie() {
+
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
