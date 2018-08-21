0d9411be4444f3f1b43a731d49b0507f13aa99de
kolheamey99@gmail.com <kolheamey99@gmail.com>
2018-08-21 10:32:05 +0530

    UserFrame

---
+++ src/com/ameykolhe/bookmymovie/userInterface/UserFrame.java
@@ -0,0 +1,39 @@
+package com.ameykolhe.bookmymovie.userInterface;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+import javax.swing.JFrame;
+
+public class UserFrame extends JFrame implements Runnable{
+
+	private static final long serialVersionUID = 1L;
+
+	Socket s;
+	
+	DataInputStream dis;
+	DataOutputStream dos;
+	ObjectOutputStream oos;
+	ObjectInputStream ois;
+
+	public UserFrame(Socket s,DataInputStream dis,DataOutputStream dos,ObjectInputStream ois,ObjectOutputStream oos , String userName) {
+		super("User" + userName);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(0, 0, 450, 300);		
+		
+		this.dis = dis;
+		this.dos = dos;
+		this.ois = ois;
+		this.oos = oos;
+		this.s = s;
+		
+	}
+
+	@Override
+	public void run() {
+		setVisible(true);
+	}
+}
