24ade4f9a2468f7f0990ff0d9529910f25ba693c
kolheamey99@gmail.com <kolheamey99@gmail.com>
2018-08-21 10:31:55 +0530

    OwnerFrame

---
+++ src/com/ameykolhe/bookmymovie/userInterface/OwnerFrame.java
@@ -0,0 +1,39 @@
+package com.ameykolhe.bookmymovie.userInterface;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+import javax.swing.JFrame;
+
+public class OwnerFrame extends JFrame implements Runnable{
+
+	private static final long serialVersionUID = 1L;
+
+	Socket s;
+	
+	DataInputStream dis;
+	DataOutputStream dos;
+	ObjectOutputStream oos;
+	ObjectInputStream ois;
+
+	public OwnerFrame(Socket s,DataInputStream dis,DataOutputStream dos,ObjectInputStream ois,ObjectOutputStream oos , String userName) {
+		super("Owner" + userName);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(0, 0, 450, 300);		
+		
+		this.dis = dis;
+		this.dos = dos;
+		this.ois = ois;
+		this.oos = oos;
+		this.s = s;
+		
+	}
+
+	@Override
+	public void run() {
+		setVisible(true);
+	}
+}
